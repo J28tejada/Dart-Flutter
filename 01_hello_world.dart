5be17de605ad21f30ac1a1052ab32b481ed837bb
j28tejada <josuetejadaromero@gmail.com>
2025-04-29 17:52:22 -0400

    Variables en Dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+  //  var myName = 'Josue';
+  //  String myName  = 'short string';
+  //  late final myName = 'Josue';
+  
+    const myName = 'the goat';
+  
+    print('I am: $myName');
+    print('I am: ${myName.toUpperCase()}');
+    print('Number ${1+1}');
+    
+}
