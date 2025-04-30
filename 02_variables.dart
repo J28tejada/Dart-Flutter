48840672d6fb9f17ee0068c2d3cca67dd28db06d
j28tejada <josuetejadaromero@gmail.com>
2025-04-30 10:44:05 -0400

    Tipos de variables en Dart

---
+++ 02_variables.dart
@@ -0,0 +1,19 @@
+void main() {
+  
+  final String pokemon = 'Raptor';
+  final int hp = 100;  
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor'];
+  final sprites = <String>['Raptor.png', 'Rex.png'];
+    
+    
+    
+    print("""
+    $pokemon
+    $hp
+    $isAlive
+    $abilities
+    $sprites
+    
+    """);
+}
