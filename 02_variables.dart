784ff3270d40b04c8009261fccbb37337c7fef5d
j28tejada <josuetejadaromero@gmail.com>
2025-05-01 08:54:33 -0400

    dynamic type

--- 02_variables.dart
+++ 02_variables.dart
@@ -1,19 +1,26 @@
 void main() {
-  
   final String pokemon = 'Raptor';
-  final int hp = 100;  
+  final int hp = 100;
   final bool isAlive = true;
   final List<String> abilities = ['impostor'];
   final sprites = <String>['Raptor.png', 'Rex.png'];
-    
-    
-    
-    print("""
+
+  // dynamic == null
+
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5, 6];
+  errorMessage = {1, 2, 3, 4, 5, 6};
+  errorMessage = () => true;
+  errorMessage = null;
+
+  print("""
     $pokemon
     $hp
     $isAlive
     $abilities
     $sprites
+    $errorMessage
     
     """);
 }
