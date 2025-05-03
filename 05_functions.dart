757522ae58cf42588cec3aa5ba6b3e99f89ba013
j28tejada <josuetejadaromero@gmail.com>
2025-05-03 17:49:31 -0400

    functions-p2

--- 05_functions.dart
+++ 05_functions.dart
@@ -2,6 +2,7 @@ void main() {
   print(greetEveryone());
   print('Sum: ${addTwoNumbers(3, 9)}');
   print('Sum: ${addTwoNumbersOptional(3, 7)}');
+  print(greetPerson(name: 'Josue', message: 'Hello,'));
 }
 
 String greetEveryone() => 'Hello Everyone!';
@@ -11,4 +12,8 @@ int addTwoNumbers(int a, int b) => a + b;
 int addTwoNumbersOptional(int a, [int b = 0]) {
   //b ??= 0;
   return a + b;
+}
+
+String greetPerson({required String name, String message = 'Hi,'}) {
+  return '$message Joel';
 }
