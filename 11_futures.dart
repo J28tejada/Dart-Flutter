a637996b2d738a10075193cc4030d4f1a2171432
test1 <macbookpro@MacBook-Pro-de-macbookpro.local>
2025-05-12 01:39:53 -0400

    futures

--- 11_futures.dart
+++ 11_futures.dart
@@ -1,19 +1,18 @@
-void main() {
+void main() async {
   print('Inicio del programa');
 
-  httpGet('https://josue-tejada.com/flutter')
-      .then((value) {
-        print(value);
-      })
-      .catchError((err) {
-        print('Error: $err');
-      });
+  try {
+    final value = await httpGet('HTTPS://josue-tejada.com/flutter');
+    print(value);
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
 
   print('Fin del programa');
 }
 
-Future<String> httpGet(String url) {
-  return Future.delayed(const Duration(seconds: 1), () {
-    throw 'Error en la peticion http';
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1), () {
+    throw 'Error en la peticion';
   });
 }
